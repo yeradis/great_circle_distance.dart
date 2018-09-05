83d923489d817ec9ef57d45fc509c51d4aa0a222
Phil Seeley <phil.seeley@gmail.com>
2018-09-05 19:58:34 +1000

    The dart math package has removed some deprecated constants, including PI.

--- lib/src/formula/spherical_lawofcosines.dart
+++ lib/src/formula/spherical_lawofcosines.dart
@@ -7,7 +7,7 @@ class Spherical_LawOfCosines {
             cos(lat1) * cos(lat2) *
             cos(lon2 - lon1)
         );
-        if (distance < 0) distance = distance + PI;
+        if (distance < 0) distance = distance + pi;
 
         var EarthRadius = 6378137.0; // WGS84 major axis
         return EarthRadius * distance;
